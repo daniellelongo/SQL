88184cb39e9d919bdc54b5e16362a448e0b004f2
Danielle Longo <d.longo120@gmail.com>
2022-06-03 17:10:23 -0400

    Create Spotify Analytics.sql

---
+++ Spotify Analytics.sql
@@ -0,0 +1,53 @@
+--For this project, I downloaded Spotify data from Kaggle, created a table to insert the Spotify data into and performed analytics on the data using SQL. 
+
+--Using the pre-existing columns in the data from kaggle, I created a table and added relevent data types and constraints.
+
+CREATE TABLE BIT_DB.SpotifyData (
+id integer PRIMARY KEY,
+artist_name varchar NOT NULL,
+track_name varchar NOT NULL,
+track_id varchar NOT NULL,
+popularity integer NOT NULL,
+danceability decimal(4,3) NOT NULL,
+energy decimal(4,3) NOT NULL,
+key integer NOT NULL,
+loudness decimal(5,3) NOT NULL,
+mode integer NOT NULL,
+speechiness decimal(5,4) NOT NULL,
+acousticness decimal(6,5) NOT NULL,
+instrumentalness text NOT NULL,
+liveness decimal(5,4) NOT NULL,
+valence decimal(4,3) NOT NULL,
+tempo decimal(6,3) NOT NULL,
+duration_ms integer NOT NULL,
+time_signature integer NOT NULL 
+)
+
+--Once the table was created I inserted the Spotify Data .csv into the table.
+
+--Finally, I explored the data using the following SQL: 
+
+--Determine the Top 10 songs and corresponding artists by popularity
+SELECT track_name, artist_name, popularity FROM BIT_DB.SpotifyData 
+ORDER BY popularity DESC LIMIT 10;
+
+--Which song has the most energy and the least energy?
+SELECT track_name, MAX(energy) FROM BIT_DB.SpotifyData;
+SELECT track_name, MIN(energy) FROM BIT_DB.SpotifyData;
+
+--what is the average length of song in minutes
+SELECT ROUND(((AVG(duration_ms)/1000)/60),2) AS duration_mins FROM BIT_DB.SpotifyData;
+
+--How many songs feature another artist? 
+SELECT COUNT(track_name) FROM BIT_DB.SpotifyData WHERE track_name LIKE '%feat.%';
+
+--which artist has the most danceable song?
+SELECT artist_name, track_name, MAX(danceability) FROM BIT_DB.SpotifyData;
+
+--Top 5 songs with higher than average loudness?
+SELECT track_name, loudness FROM BIT_DB.SpotifyData
+WHERE loudness > (SELECT AVG(loudness) FROM BIT_DB.SpotifyData)
+ORDER BY loudness DESC LIMIT 5;
+
+--Which song involves the most spoken words?
+SELECT track_name, MAX(speechiness) FROM BIT_DB.SpotifyData; 
