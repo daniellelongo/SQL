0798773fe4ffb7cbfafec8763d9ca2ff6e4bcd22
Danielle Longo <d.longo120@gmail.com>
2023-05-03 13:40:08 -0400

    Update Furniture Transaction Data with Stats.sql

--- Furniture Transaction Data with Stats.sql
+++ Furniture Transaction Data with Stats.sql
@@ -1,7 +1,9 @@
 --Query database holding Furniture Store Transaction data to answer business questions.
 
 --Top five countries with the highest number of transactions
-SELECT country, COUNT(transaction_date) AS number_of_transactions  FROM sales GROUP BY country ORDER BY COUNT(transaction_date) DESC LIMIT 5;
+SELECT country, COUNT(transaction_date) AS number_of_transactions FROM sales 
+GROUP BY country 
+ORDER BY COUNT(transaction_date) DESC LIMIT 5;
 
 --Highest priced product within a transaction
 SELECT product, price, MAX(price) FROM sales;
@@ -13,19 +15,29 @@ SELECT product, price, MIN(price) FROM sales;
 SELECT AVG(price) FROM sales;
 
 --Total sales in each country
-SELECT country, SUM(price) AS total_sales FROM sales GROUP BY country;
+SELECT country, SUM(price) AS total_sales FROM sales 
+GROUP BY country;
 
 --Top five countries with the highest total sales?
-SELECT country, SUM(price) as total_sales FROM sales GROUP BY country ORDER BY SUM(price) DESC LIMIT 5;
+SELECT country, SUM(price) as total_sales FROM sales 
+GROUP BY country 
+ORDER BY SUM(price) DESC LIMIT 5;
 
 --Least profitable country?
-SELECT country, SUM(price) as total_sales FROM sales GROUP BY country ORDER BY SUM(price) ASC LIMIT 1;
+SELECT country, SUM(price) as total_sales FROM sales 
+GROUP BY country 
+ORDER BY SUM(price) ASC LIMIT 1;
 
 --Display the number of transactions for each payment type
-SELECT payment_type, COUNT(payment_type) FROM sales GROUP BY  payment_type ORDER BY COUNT(payment_type) DESC
+SELECT payment_type, COUNT(payment_type) FROM sales 
+GROUP BY payment_type 
+ORDER BY COUNT(payment_type) DESC;
 
 --What is the total sales of all transactions from each payment type?
-SELECT payment_type, SUM(price) FROM sales GROUP BY payment_type;
+SELECT payment_type, SUM(price) FROM sales 
+GROUP BY payment_type;
 
 --Display the total sales in all transactions from the cities including their states, in United States 
-SELECT city, state, SUM(price) FROM sales WHERE country = "United states" GROUP BY city;
+SELECT city, state, SUM(price) FROM sales 
+WHERE country = "United states" 
+GROUP BY city;
